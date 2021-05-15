289f36907d14faae0e31b27f9f74a4c6032fd949
Luis Chavez <luischavezcald@gmail.com>
2021-05-15 09:48:39 -0400

    feat: add new property to item model

--- Data/Item.cs
+++ Data/Item.cs
@@ -1,8 +1,13 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Inventory.Data {
   public class Item
   {
     public int ItemId { get; set; }
     public string Name { get; set; }
     public int Quantity { get; set; }
+    [Column("created_at")]
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
   }
 }
